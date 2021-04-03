33268ae70c607bef9813129c37da15a422cc2e4d
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-03 15:19:09 +0100

    I build an app that lets users generate a QR code for any url and then save that code to their photo library.

---
+++ QR Code Generator/QRCodeGenerator.swift
@@ -0,0 +1,37 @@
+//
+//  QRCodeGenerator.swift
+//  QR Code Generator
+//
+//  Created by elhajjaji on 3/4/2021.
+//
+
+import Foundation
+import CoreImage.CIFilterBuiltins
+import UIKit
+
+struct QRCodeGenerator {
+
+  private let context = CIContext()
+  private let filter = CIFilter.qrCodeGenerator()
+
+  public func generateQRCode(forUrlString urlString: String) -> QRCode? {
+    guard !urlString.isEmpty else { return nil }
+
+    let data = Data(urlString.utf8)
+    filter.setValue(data, forKey: "inputMessage")
+    let transform = CGAffineTransform(scaleX: 10, y: 10)
+
+    if let outputImage = filter.outputImage?.transformed(by: transform) {
+      if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+        let qrCode = QRCode(urlString: urlString, uiImage: UIImage(cgImage: cgImage))
+        return qrCode
+      }
+    }
+    return nil
+  }
+}
+
+struct QRCode {
+  let urlString: String
+  let uiImage: UIImage
+}
