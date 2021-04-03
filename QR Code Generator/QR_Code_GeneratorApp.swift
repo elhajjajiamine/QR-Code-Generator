efe7b1c6172c3e25b3b688534c81ab10a7ef9cb4
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-03 12:52:38 +0100

    Initial Commit

---
+++ QR Code Generator/QR_Code_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Code_GeneratorApp.swift
+//  QR Code Generator
+//
+//  Created by elhajjaji on 3/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct QR_Code_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
